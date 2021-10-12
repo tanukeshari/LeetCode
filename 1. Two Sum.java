a11b04632d8242d97614f6dc495befa3d8cbc0a6
Ruiyue Wang <phynew@hotmail.com>
2021-10-12 18:06:03 -0400

    Update 1. Two Sum.java

--- 1. Two Sum.java
+++ 1. Two Sum.java
@@ -23,9 +23,9 @@
 // Output: [0,1]
  
 // Constraints:
-// 2 <= nums.length <= 104
-// -109 <= nums[i] <= 109
-// -109 <= target <= 109
+// 2 <= nums.length <= 10^4
+// -10^9 <= nums[i] <= 10^9
+// -10^9 <= target <= 10^9
 // Only one valid answer exists.
  
 // Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?
@@ -50,3 +50,33 @@ class Solution {
 }
 
 // TC: O(n^2); SC: O(1)
+// Success
+// Details 
+// Runtime: 72 ms, faster than 17.25% of Java online submissions for Two Sum.
+// Memory Usage: 41.6 MB, less than 35.48% of Java online submissions for Two Sum.
+
+
+// Solution 2
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> complement = new HashMap<>();
+        
+        for (int i = 0; i < nums.length; ++i) {
+            int temp = target - nums[i];
+            
+            if (complement.containsKey(temp)) {
+                return new int[] {complement.get(temp), i};
+            }
+            
+            complement.put(nums[i], i);
+        }
+        
+        return null;
+    }
+}
+
+// TC: O(n); SC: O(n)
+// Success
+// Details 
+// Runtime: 2 ms, faster than 76.47% of Java online submissions for Two Sum.
+// Memory Usage: 42.6 MB, less than 18.77% of Java online submissions for Two Sum.
