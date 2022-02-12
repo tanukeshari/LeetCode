e23a11d4400a598d330861946f24594e869b83c4
Ruiyue Wang <phynew@hotmail.com>
2022-02-12 15:40:38 -0500

    Create 696. Count Binary Substrings.java

---
+++ 696. Count Binary Substrings.java
@@ -0,0 +1,55 @@
+// Easy
+// Give a binary string s, 
+// return the number of non-empty substrings that have the same number of 0's and 1's, 
+// and all the 0's and all the 1's in these substrings are grouped consecutively.
+
+// Substrings that occur multiple times are counted the number of times they occur.
+
+// Example 1:
+// Input: s = "00110011"
+// Output: 6
+  
+// Explanation: 
+// There are 6 substrings that have equal number of consecutive 1's and 0's: "0011", "01", "1100", "10", "0011", and "01".
+// Notice that some of these substrings repeat and are counted the number of times they occur.
+// Also, 
+// "00110011" is not a valid substring because all the 0's (and 1's) are not grouped together.
+  
+// Example 2:
+// Input: s = "10101"
+// Output: 4
+  
+// Explanation: 
+// There are 4 substrings: "10", "01", "10", "01" that have equal number of consecutive 1's and 0's.
+ 
+// Constraints:
+// 1 <= s.length <= 105
+// s[i] is either '0' or '1'.
+  
+// Solution 1
+class Solution {
+    public int countBinarySubstrings(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        
+        int cnt = 0;
+        int len = s.length();
+        
+        for (int i = 0; i < len - 1; i++) {
+            char chI = s.charAt(i);
+            char chI1 = s.charAt(i + 1);
+            if (chI != chI1) {
+                int j = 0;
+                
+                while (0 <= i - j && i + 1 + j < len && chI == s.charAt(i - j) && chI1 == s.charAt(i + 1 + j)) {
+                    cnt++;
+                    j++;
+                }
+            }
+        }
+        
+        return cnt;
+    }
+}
+// TC: O(n^2); SC: O(1)
