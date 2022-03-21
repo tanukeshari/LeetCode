21de8381318805e87aab0df1f54d41091f884824
Ruiyue Wang <phynew@hotmail.com>
2022-03-20 20:06:09 -0400

    Create 1644. Lowest Common Ancestor of a Binary Tree II.java

---
+++ 1644. Lowest Common Ancestor of a Binary Tree II.java
@@ -0,0 +1,120 @@
+// Medium
+// Given the root of a binary tree, 
+// return the lowest common ancestor (LCA) of two given nodes, 
+// p and q. 
+// If either node p or q does not exist in the tree, 
+// return null. 
+// All values of the nodes in the tree are unique.
+
+// According to the definition of LCA on Wikipedia: 
+// "The lowest common ancestor of two nodes p and q in a binary tree T is the lowest node that has both p and q as descendants (where we allow a node to be a descendant of itself)".
+// A descendant of a node x is a node y that is on the path from node x to some leaf node.
+
+// Example 1:
+// Input: 
+// root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+// Output: 3
+  
+// Explanation:
+// The LCA of nodes 5 and 1 is 3.
+  
+// Example 2:
+// Input: 
+// root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+// Output: 5
+  
+// Explanation: 
+// The LCA of nodes 5 and 4 is 5. 
+// A node can be a descendant of itself according to the definition of LCA.
+  
+// Example 3:
+
+// Input: 
+// root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 10
+// Output: null
+  
+// Explanation: 
+// Node 10 does not exist in the tree, 
+// so return null.
+ 
+// Constraints:
+// The number of nodes in the tree is in the range [1, 104].
+// -109 <= Node.val <= 109
+// All Node.val are unique.
+// p != q
+ 
+// Follow up: 
+// Can you find the LCA traversing the tree, 
+// without checking nodes existence?
+
+// Solution
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        boolean[] pFind = new boolean[] {false};
+        boolean[] qFind = new boolean[] {false};
+        
+        TreeNode ans = helper(root, p, q, pFind, qFind);
+        
+        if (pFind[0] == true && qFind[0] == true) {
+            return ans;
+        }
+        else {
+            return null;
+        }
+    }
+    
+    private TreeNode helper (TreeNode root, TreeNode p, TreeNode q, boolean[] pFind, boolean[] qFind) {
+        boolean rootFind = false;
+        
+        if (root == null) {
+            return null;
+        }
+
+        if (root == p) {
+            pFind[0] = true;
+            rootFind = true;
+
+            if (qFind[0] == true) {
+                return root;
+            }
+        }
+        
+        if (root == q) {
+            qFind[0] = true;
+            rootFind = true;
+
+            if (pFind[0] == true) {
+                return root;
+            }
+        }
+
+        TreeNode leftChild = helper(root.left, p, q, pFind, qFind);
+        TreeNode rightChild = helper(root.right, p, q, pFind, qFind);
+        
+        if (pFind[0] == false && qFind[0] == false) {
+            return null;
+        }
+        else {
+            if (rootFind == true || leftChild != null && rightChild != null) {
+                return root;
+            }
+            else {
+                return leftChild == null ? rightChild : leftChild;
+            }
+        }
+    }
+}
+// TC: O(n); SC: O(height)
+// Success
+// Details 
+// Runtime: 7 ms, faster than 98.71% of Java online submissions for Lowest Common Ancestor of a Binary Tree II.
+// Memory Usage: 45.4 MB, less than 82.03% of Java online submissions for Lowest Common Ancestor of a Binary Tree II.
