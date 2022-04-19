c9e6d806bb05401e254c79a5a1195c702f86b60d
Ruiyue Wang <phynew@hotmail.com>
2022-04-19 12:52:23 -0400

    Update 53. Maximum Subarray.py

--- 53. Maximum Subarray.py
+++ 53. Maximum Subarray.py
@@ -1,17 +1,21 @@
 # Easy
-
-# Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+# Given an integer array nums, 
+# find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
 
 # Example:
-
-# Input: [-2,1,-3,4,-1,2,1,-5,4],
-# Output: 6
-# Explanation: [4,-1,2,1] has the largest sum = 6.
+# Input: 
+# [-2,1,-3,4,-1,2,1,-5,4],
+# Output: 
+# 6
+# Explanation: 
+# [4,-1,2,1] has the largest sum = 6.
 
 # Follow up:
+# If you have figured out the O(n) solution, 
+# try coding another solution using the divide and conquer approach, 
+# which is more subtle.
 
-# If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
-
+# Solution 1
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         res = float('-inf')
@@ -21,7 +25,7 @@ class Solution:
             if temp > res:
                 res = temp
         return res
-        
+# TC: O(n); SC: O(1)
 # Success
 # Details 
 # Runtime: 104 ms, faster than 18.65% of Python3 online submissions for Maximum Subarray.
