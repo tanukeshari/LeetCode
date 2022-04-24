9c6076cb60ff6d27d61cb8b6e00fa8f5df863e71
Ruiyue Wang <phynew@hotmail.com>
2022-04-24 01:05:27 -0400

    Create 86. Partition List.java

---
+++ 86. Partition List.java
@@ -0,0 +1,69 @@
+// Medium
+// Given the head of a linked list and a value x, 
+// partition it such that all nodes less than x come before nodes greater than or equal to x.
+
+// You should preserve the original relative order of the nodes in each of the two partitions.
+  
+// Example 1:
+// Input: 
+// head = [1,4,3,2,5,2], x = 3
+// Output: 
+// [1,2,2,4,3,5]
+
+// Example 2:
+// Input: 
+// head = [2,1], x = 2
+// Output: 
+// [1,2]
+ 
+// Constraints:
+// The number of nodes in the list is in the range [0, 200].
+// -100 <= Node.val <= 100
+// -200 <= x <= 200
+  
+// Solution
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        if (head == null) {
+            return head;
+        }
+        
+        ListNode firstHalf = new ListNode();
+        ListNode firstCur = firstHalf;
+        ListNode lastHalf = new ListNode();
+        ListNode lastCur = lastHalf;
+        
+        while (head != null) {
+            if (head.val < x) {
+                firstCur.next = head;
+                head = head.next;
+                firstCur = firstCur.next;
+            }
+            else {
+                lastCur.next = head;
+                head = head.next;
+                lastCur = lastCur.next;
+            }
+        }
+        
+        firstCur.next = lastHalf.next;
+        lastCur.next = null;
+        
+        return firstHalf.next;
+    }
+}
+// TC: O(n); SC: O(1)
+// Success
+// Details 
+// Runtime: 0 ms, faster than 100.00% of Java online submissions for Partition List.
+// Memory Usage: 43.3 MB, less than 10.10% of Java online submissions for Partition List.
