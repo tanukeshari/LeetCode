8fbb61836209caccb58db1120e42e83bd642189f
Ruiyue Wang <phynew@hotmail.com>
2022-04-25 14:47:47 -0400

    Update 15. 3Sum.java

--- 15. 3Sum.java
+++ 15. 3Sum.java
@@ -1,22 +1,20 @@
 // Medium
-
-// Given an array nums of n integers, are there elements a, b, c in nums such that a + b + c = 0? 
+// Given an array nums of n integers, 
+// are there elements a, b, c in nums such that a + b + c = 0? 
 // Find all unique triplets in the array which gives the sum of zero.
 
 // Note:
-
 // The solution set must not contain duplicate triplets.
 
 // Example:
-
 // Given array nums = [-1, 0, 1, 2, -1, -4],
-
 // A solution set is:
 // [
 //   [-1, 0, 1],
 //   [-1, -1, 2]
 // ]
 
+// Solution
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
@@ -45,7 +43,7 @@ class Solution {
         }
     }
 }
-
+// 
 // Success
 // Details 
 // Runtime: 33 ms, faster than 41.84% of Java online submissions for 3Sum.
