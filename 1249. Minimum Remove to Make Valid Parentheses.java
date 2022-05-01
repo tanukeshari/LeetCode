deb0ef72b828e60aebd3642a6ca2186ff48738f3
Ruiyue Wang <phynew@hotmail.com>
2022-04-30 22:36:29 -0400

    Update 1249. Minimum Remove to Make Valid Parentheses.java

--- 1249. Minimum Remove to Make Valid Parentheses.java
+++ 1249. Minimum Remove to Make Valid Parentheses.java
@@ -1,38 +1,49 @@
 // Medium
-
 // Given a string s of '(' , ')' and lowercase English characters. 
+// Your task is to remove the minimum number of parentheses ( '(' or ')', 
+// in any positions ) so that the resulting parentheses string is valid and return any valid string.
 
-// Your task is to remove the minimum number of parentheses ( '(' or ')', in any positions ) so that the resulting parentheses string is valid and return any valid string.
-
-// Formally, a parentheses string is valid if and only if:
-
+// Formally, 
+// a parentheses string is valid if and only if:
 // It is the empty string, contains only lowercase characters, or
 // It can be written as AB (A concatenated with B), where A and B are valid strings, or
 // It can be written as (A), where A is a valid string.
  
 // Example 1:
-// Input: s = "lee(t(c)o)de)"
-// Output: "lee(t(c)o)de"
-// Explanation: "lee(t(co)de)" , "lee(t(c)ode)" would also be accepted.
+// Input: 
+// s = "lee(t(c)o)de)"
+// Output: 
+// "lee(t(c)o)de"
+
+// Explanation: 
+// "lee(t(co)de)" , "lee(t(c)ode)" would also be accepted.
 
 // Example 2:
-// Input: s = "a)b(c)d"
-// Output: "ab(c)d"
+// Input: 
+// s = "a)b(c)d"
+// Output: 
+// "ab(c)d"
 
 // Example 3:
-// Input: s = "))(("
-// Output: ""
-// Explanation: An empty string is also valid.
+// Input: 
+// s = "))(("
+// Output: 
+// ""
+
+// Explanation: 
+// An empty string is also valid.
 
 // Example 4:
-// Input: s = "(a(b(c)d)"
-// Output: "a(b(c)d)"
+// Input: 
+// s = "(a(b(c)d)"
+// Output: 
+// "a(b(c)d)"
  
 // Constraints:
 // 1 <= s.length <= 10^5
 // s[i] is one of  '(' , ')' and lowercase English letters.
 
-// #1, Using a Stack and String Builder
+// Solution 1, Using a Stack and String Builder
 class Solution {
     public String minRemoveToMakeValid(String s) {
         Set<Integer> indexesToRemove = new HashSet<>();
@@ -68,13 +79,13 @@ class Solution {
         return sb.toString();
     }
 }
-
+// TC: O(n); SC: O(n)
 // Success
 // Details 
 // Runtime: 52 ms, faster than 13.52% of Java online submissions for Minimum Remove to Make Valid Parentheses.
 // Memory Usage: 52.3 MB, less than 5.08% of Java online submissions for Minimum Remove to Make Valid Parentheses.
 
-// #2 Two Parse String Builde
+// Solution 2, Two Parse String Builde
 class Solution {
    private StringBuilder removeInvalidClosing(CharSequence string, char open, char close) {
        StringBuilder sb = new StringBuilder();
@@ -105,13 +116,13 @@ class Solution {
         return result.reverse().toString();
     }
 }
-
+// TC: O(n); SC: O(n)
 // Success
 // Details 
 // Runtime: 33 ms, faster than 31.19% of Java online submissions for Minimum Remove to Make Valid Parentheses.
 // Memory Usage: 47.8 MB, less than 5.08% of Java online submissions for Minimum Remove to Make Valid Parentheses.
 
-// #3Shortened Two Parse String Builder
+// Solution 3, Shortened Two Parse String Builder
 class Solution {
    public String minRemoveToMakeValid(String s) {
        int openSeen = 0, balance = 0;
@@ -153,8 +164,69 @@ class Solution {
        return result.toString();
    }
 }
-
+// TC: O(n); SC: O(n)
 // Success
 // Details 
 // Runtime: 27 ms, faster than 40.99% of Java online submissions for Minimum Remove to Make Valid Parentheses.
 // Memory Usage: 48.1 MB, less than 5.08% of Java online submissions for Minimum Remove to Make Valid Parentheses.
+
+// Solution 4
+class Solution {
+    public String minRemoveToMakeValid(String s) {
+        if (s == null || s.length() == 0) {
+            return s;
+        }
+        
+        StringBuilder temp = new StringBuilder();
+        int cnt = 0;
+        int nLeft = 0;
+        
+        for (int i = 0; i < s.length(); i++) {
+            char chL = s.charAt(i);
+            
+            if (chL == '(') {
+                temp.append(chL);
+                nLeft++;
+                cnt++;                
+            }
+            else if (chL == ')') {
+                if (cnt > 0) {
+                    temp.append(chL);
+                    cnt--;
+                }
+            }
+            else {
+                temp.append(chL);
+            }
+        }
+        
+        if (cnt == 0) {
+            return temp.toString();
+        }
+        
+        StringBuilder ans = new StringBuilder();
+        int keepLeft = nLeft - cnt;
+        
+        for (int j = 0; j < temp.length(); j++) {
+            char chR = temp.charAt(j);
+            
+            if (chR == '(') {
+                if (keepLeft > 0) {
+                    keepLeft--;
+                }
+                else {
+                    continue;
+                }
+            }
+            
+            ans.append(chR);
+        }
+        
+        return ans.toString();
+    }
+}
+// TC: O(n); SC: O(n)
+// Success
+// Details 
+// Runtime: 21 ms, faster than 79.44% of Java online submissions for Minimum Remove to Make Valid Parentheses.
+// Memory Usage: 51.6 MB, less than 55.04% of Java online submissions for Minimum Remove to Make Valid Parentheses.
