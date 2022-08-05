3e77a8c11cea1ac5028e04df30d9c0ed2028968a
Ruiyue Wang <phynew@hotmail.com>
2022-08-05 01:12:19 -0400

    Update 347. Top K Frequent Elements.java

--- 347. Top K Frequent Elements.java
+++ 347. Top K Frequent Elements.java
@@ -96,3 +96,78 @@ class Solution {
 // Details 
 // Runtime: 28 ms, faster than 18.36% of Java online submissions for Top K Frequent Elements.
 // Memory Usage: 49.3 MB, less than 83.15% of Java online submissions for Top K Frequent Elements.
+
+// Solution 3
+class Solution {
+    Map<Integer, Integer> frq;
+    List<Integer> keys;
+    
+    public int[] topKFrequent(int[] nums, int k) {
+        frq = new HashMap<>();
+        
+        for (int num : nums) {
+            frq.put(num, frq.getOrDefault(num, 0) + 1);
+        }
+        
+        keys = new ArrayList<>(frq.keySet());
+        
+        quickSelect(keys.size(), k, 0, keys.size() - 1);
+             
+        int[] ans = new int[k];
+        
+        for (int i = keys.size() - k; i < keys.size(); i++) {
+            ans[i - keys.size() + k] = keys.get(i);
+        }
+        
+        return ans;
+    }
+    
+    private void quickSelect(int len, int k, int lt, int rt) {
+        if (lt == rt) {
+            return;
+        }
+        
+        Random rand = new Random();
+        int pivot = lt + rand.nextInt(rt - lt);
+        
+        pivot = partition(lt, rt, pivot);
+        
+        if (pivot == len - k) {
+            return;
+        }
+        else if (pivot < len - k) {
+            quickSelect(len, k, pivot + 1, rt);
+        }
+        else {
+            quickSelect(len, k, lt, pivot - 1);
+        }
+    }
+    
+    private int partition(int lt, int rt, int pivot) {
+        swap(keys, pivot, rt);
+        
+        int idx = lt;
+        
+        for (int i = lt; i < rt; i++) {
+            if (frq.get(keys.get(i)) < frq.get(keys.get(rt))) {
+                swap(keys, i, idx);
+                idx++;
+            }
+        }
+        
+        swap(keys, idx, rt);
+        
+        return idx;     
+    }
+    
+    private void swap(List<Integer> keys, int i, int j) {
+        int temp = keys.get(i);
+        keys.set(i, keys.get(j));
+        keys.set(j, temp);
+    }
+}
+// TC: average O(n); SC: O(n)
+// Success
+// Details 
+// Runtime: 15 ms, faster than 76.40% of Java online submissions for Top K Frequent Elements.
+// Memory Usage: 50.2 MB, less than 72.43% of Java online submissions for Top K Frequent Elements.
